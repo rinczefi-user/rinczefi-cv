f1c526bdacfd9f9f3a3431d4ff4967ec18045f20
rinczefi-user <inczefirobert@yahoo.com>
2024-08-31 01:22:57 +0300

    refactor: swap fields

--- src/App.tsx
+++ src/App.tsx
@@ -24,8 +24,8 @@ const App: React.FC = () => {
             <Header />
             <AboutMe />
             <Experience />
-            <Education />
             <Skills />
+            <Education />
           </StyledPaper>
         </StyledBox>
       </StyledBox>
